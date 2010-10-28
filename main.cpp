be0d62bd8a5930a867cd7031ad2768a61c12122d
axeloide <axel.rohde@gmail.com>
2010-10-28 22:43:55 +0200

    Beautified source.

--- main.cpp
+++ main.cpp
@@ -7,22 +7,21 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-	int res;
-	unsigned char buf[255];
-	#define MAX_STR 255
-	wchar_t wstr[MAX_STR];
-	hid_device *handle;
+int main(int argc, char* argv[]) {
+    int res;
+    unsigned char buf[255];
+#define MAX_STR 255
+    wchar_t wstr[MAX_STR];
+    hid_device *handle;
 
 
     printf("Looking for a device with a certain VID/PID pair...");
     fflush(stdout);
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	do {
+    // Open the device using the VID, PID,
+    // and optionally the Serial number.
+    do {
         handle = hid_open(0x03eb, 0xaaaa, NULL);
-	} while (handle==NULL);
+    } while (handle==NULL);
 
     printf("Found!\n");
 
@@ -31,27 +30,27 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-	// Read the Manufacturer String
-	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	if (res<0) return -1;
-	printf("Manufacturer String: %ls\n", wstr);
+    // Read the Manufacturer String
+    res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+    if (res<0) return -1;
+    printf("Manufacturer String: %ls\n", wstr);
 
-	// Read the Product String
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (res<0) return -1;
-	printf("Product String: %ls\n", wstr);
+    // Read the Product String
+    res = hid_get_product_string(handle, wstr, MAX_STR);
+    if (res<0) return -1;
+    printf("Product String: %ls\n", wstr);
 
-	// Read the Serial Number String
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	if (res<0) return -1;
-	printf("Serial Number String: %ls\n", wstr);
+    // Read the Serial Number String
+    res = hid_get_serial_number_string(handle, wstr, MAX_STR);
+    if (res<0) return -1;
+    printf("Serial Number String: %ls\n", wstr);
 
 
-	// Send a Feature Report to the device: SetColours
-	// buf[0] = 0x06; // First byte is report number
-	// buf[1] = 0x07;
-	// buf[2] = 0x00;
-	// res = hid_send_feature_report(handle, buf, 3);
+    // Send a Feature Report to the device: SetColours
+    // buf[0] = 0x06; // First byte is report number
+    // buf[1] = 0x07;
+    // buf[2] = 0x00;
+    // res = hid_send_feature_report(handle, buf, 3);
 
 
     while (1) {
@@ -79,6 +78,6 @@ int main(int argc, char* argv[])
 
     //getchar();
 
-	return 0;
+    return 0;
 }
 
