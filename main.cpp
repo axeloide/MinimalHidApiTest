2d1368d626862733be68487c4ac627e6679e667a
axeloide <axel.rohde@gmail.com>
2010-10-28 16:38:38 +0200

    Implemented a multiple thread test case.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,31 @@
 
 
 #include "hidapi.h"
-
+#include <pthread.h>
 
 using namespace std;
+
+pthread_t thread;
+pthread_barrier_t barrier; /* Ensures correct startup sequence */
+int shutdown_thread;
+
+
+static void *read_thread(void *param)
+{
+	hid_device *dev = (hid_device *) param;
+	unsigned char buf[255];
+
+
+	// Notify the main thread that the read thread is up and running.
+	pthread_barrier_wait(&barrier);
+
+	/* Handle all the events. */
+	while (!shutdown_thread) {
+        hid_read(dev, buf , sizeof(buf));
+	}
+
+	return NULL;
+}
 
 int main(int argc, char* argv[])
 {
@@ -14,81 +36,86 @@ int main(int argc, char* argv[])
 	#define MAX_STR 255
 	wchar_t wstr[MAX_STR];
 	hid_device *handle;
-	int i;
-
-	// Enumerate and print the HID devices on the system
-	struct hid_device_info *devs, *cur_dev;
-
-	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;
-	while (cur_dev) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
-			cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
 
 
+    printf("Looking for a device with a certain VID/PID pair...");
+    fflush(stdout);
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
-	handle = hid_open(0x03eb, 0xaaaa, NULL);
+	do {
+        handle = hid_open(0x03eb, 0xaaaa, NULL);
+	} while (handle==NULL);
+
+    printf("Found!\n");
+
+    if (hid_set_nonblocking(handle,1) != 0) {
+        printf("Could not set device handle to non-blocking!");
+        return -1;
+    }
+
+    // Start a Thread that will poll for input reports
+    shutdown_thread = 0;
+    pthread_barrier_init(&barrier, NULL, 2);
+    pthread_create(&thread, NULL, read_thread, handle);
+    // Wait here for the read thread to be initialized.
+    pthread_barrier_wait(&barrier);
+
 
 	// Read the Manufacturer String
 	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+	if (res<0) return -1;
 	printf("Manufacturer String: %ls\n", wstr);
 
 	// Read the Product String
 	res = hid_get_product_string(handle, wstr, MAX_STR);
+	if (res<0) return -1;
 	printf("Product String: %ls\n", wstr);
 
 	// Read the Serial Number String
 	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	printf("Serial Number String: %ls", wstr);
-	printf("\n");
+	if (res<0) return -1;
+	printf("Serial Number String: %ls\n", wstr);
 
 
 	// Send a Feature Report to the device: SetColours
-	buf[0] = 0x06; // First byte is report number
-	buf[1] = 0x07;
-	buf[2] = 0x00;
-	res = hid_send_feature_report(handle, buf, 3);
+	// buf[0] = 0x06; // First byte is report number
+	// buf[1] = 0x07;
+	// buf[2] = 0x00;
+	// res = hid_send_feature_report(handle, buf, 3);
 
 
-	// Send a Feature Report to the device
-	buf[0] = 0x02; // First byte is report number
-	buf[1] = 0x00;
-	buf[2] = 0x01;
-	buf[3] = 0x00;
-	buf[4] = 0x00;
-	buf[5] = 0x00;
-	buf[6] = 0x01;
-	res = hid_send_feature_report(handle, buf, 7);
+    while (1) {
+        // Read a Feature Report from the device
+        buf[0] = 0x1;
+        res = hid_get_feature_report(handle, buf, sizeof(buf));
 
-	// Read a Feature Report from the device
-	buf[0] = 0x1;
-	res = hid_get_feature_report(handle, buf, sizeof(buf));
+        if (res<0) {
+            printf("Getting feature report failed with: %d\n",res);
+            break;
+        }
 
-	// Print out the returned buffer.
-	printf("Feature Report\n   ");
-	for (i = 0; i < res; i++)
-		printf("%02hx.", buf[i]);
-	printf("\n");
+        if (res>0) {
+            printf(".");
+            fflush(stdout);
 
-	// Set the hid_read() function to be non-blocking.
-	hid_set_nonblocking(handle, 1);
+            // Print out the returned buffer.
+//            printf("Feature Report\n   ");
+//            for (int i = 0; i < res; i++)
+//                printf("0x%02hx ", buf[i]);
+//            printf("\n");
+        }
 
-	// Read requested state
-	res = hid_read(handle, buf, 46);
-	if (res < 0)
-		printf("Unable to read()\n");
+    }
 
-	// Print out the returned buffer.
-	for (i = 0; i < res; i++)
-		printf("buf[%d]: %d\n", i, buf[i]);
+    // Signal thread to stop.
+	shutdown_thread = 1;
+
+	// Wait for thread to stop
+	pthread_join(thread, NULL);
+
+    hid_close(handle);
+
+    getchar();
 
 	return 0;
 }
