1ab002c1fcf9ec129d7f2c9ec53a988f308524f4
axeloide <axel.rohde@gmail.com>
2010-10-28 23:18:39 +0200

    Cleaning up.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 #include "hidapi.h"
-#include <pthread.h>
 
 using namespace std;
 
@@ -17,8 +14,6 @@ int main(int argc, char* argv[]) {
 
     printf("Looking for a device with a certain VID/PID pair...");
     fflush(stdout);
-    // Open the device using the VID, PID,
-    // and optionally the Serial number.
     do {
         handle = hid_open(0x03eb, 0xaaaa, NULL);
     } while (handle==NULL);
@@ -46,24 +41,7 @@ int main(int argc, char* argv[]) {
     printf("Serial Number String: %ls\n", wstr);
 
 
-    // Send a Feature Report to the device: SetColours
-    // buf[0] = 0x06; // First byte is report number
-    // buf[1] = 0x07;
-    // buf[2] = 0x00;
-    // res = hid_send_feature_report(handle, buf, 3);
-
-
     while (1) {
-
-        /*
-        // Read a Feature Report from the device
-        buf[0] = 0x1;
-        res = hid_get_feature_report(handle, buf, sizeof(buf));
-        if (res<0) {
-            printf("Getting feature report failed with: %d\n",res);
-            break;
-        }
-        */
 
         // Read a Input Report from the device
         res = hid_read(handle, buf , sizeof(buf));
@@ -76,7 +54,7 @@ int main(int argc, char* argv[]) {
 
     hid_close(handle);
 
-    //getchar();
+    getchar();
 
     return 0;
 }
